67abe19a28cffc123f7eceae43987775a417701f
Pratimesh <pratimesh2004@gmail.com>
2023-07-02 15:12:09 +0530

    Modified Server.js

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');  //requires make use of dependencies here express
+const serverConfig = require('./configs/server.config');
+
 
 const app = express(); //app is the central character helps in doing everything 
 
-app.listen(3333,()=>{    //(portNo,callbackFunction)
-    console.log("Listening to port 3333 .Server Started")
+app.listen(serverConfig.PORT,()=>{    //(portNo,callbackFunction)
+    console.log(`server started on the port number ${serverConfig.PORT}`)
 })
